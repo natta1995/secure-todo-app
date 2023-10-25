492bd5d9d662a79b5225aff1db37e1697457b3a2
Natalie <natalie.hallerdal@yahoo.se>
2023-10-25 14:44:33 +0200

    Todos backend api

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,9 @@ app.use(cors());
 const usersRouter = require('./users');
 app.use('/api/users', usersRouter);
 
+const todosRouter = require('./todos'); 
+app.use('/api/todos', todosRouter);
+
 app.listen(port, () => {
   console.log(`Servern lyssnar på port ${port}`);
 });
